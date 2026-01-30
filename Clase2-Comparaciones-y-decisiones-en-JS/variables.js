785b1073a956e095b545332fa35ba82c6b3aebb9
Jesus Mares <jesusalberto.0330@itsjc.edu.mx>
2026-01-29 19:27:45 -0600

    variables y operadores

---
+++ Clase2-Comparaciones-y-decisiones-en-JS/variables.js
@@ -0,0 +1,47 @@
+//let 
+
+let mascota = 'perro';
+let mascota1 = 'gato';
+
+const numero = 23;
+
+let num1 = 3;
+let num2 = 5;
+
+let suma = num1 + num2;
+let resta = num2 - num1;
+let modulo = num1 % num2;
+let exp = num1 ** num2;
+
+console.log(suma);
+console.log(15 + 15);
+console.log(resta);
+console.log("modulo de 3 % 5", +modulo);
+console.log("exponente de 3 a la 5", +exp);
+console.log(num1 * num2);
+console.log(num2 / num1);
+
+//comparacion simple
+console.log(num1 == num2);
+console.log(10 == 10);
+console.log(12 == '12');
+
+//comparacion estricta
+console.log(23 === '23');
+
+//comparacion con operadores
+let valor1 = 20;
+let valor2 = 30;
+
+//mayor que
+console.log(valor1 > valor2);
+//mayor o igual que
+console.log(valor1 >= valor2);
+//menor que
+console.log(valor1 < valor2);
+//menor  o igual que
+console.log(valor1 <= valor2);
+//diferente que
+console.log(10 != '10');
+//diferente que estricto
+console.log(10 !== '10');
