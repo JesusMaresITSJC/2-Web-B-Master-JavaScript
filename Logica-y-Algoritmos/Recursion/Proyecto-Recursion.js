a7018489766741dd2c99d0e36fa0f5aa629599fd
Jesus Mares <jesusalberto.0330@itsjc.edu.mx>
2026-03-26 19:34:14 -0600

    Busqueda Binaria

--- Logica-y-Algoritmos/Recursion/Proyecto-Recursion.js
+++ Logica-y-Algoritmos/Recursion/Proyecto-Recursion.js
@@ -1,13 +1,10 @@
-// Lista de regalos
 const gifts = ["Muñeca", "Carro de juguete", "Rompecabezas", "Lego", "Pelota"];
 
 function findGift(gifts, giftName, index = 0) {
-    // Caso base: si llegamos al final de la lista
     if (index === gifts.length) {
         return `${giftName} no está en la lista.`;
     }
 
-    // Caso base: si encontramos el regalo
     if (gifts[index] === giftName) {
         return `${giftName} está en la posición ${index}.`;
     }
